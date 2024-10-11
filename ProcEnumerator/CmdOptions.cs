8e8431b53cebbf6f6a3ff9d8b65b53527646ea4d
bjha96 <bimalesh@gmail.com>
2024-10-11 18:04:49 +0530

    Added file header notice

--- ProcEnumerator/CmdOptions.cs
+++ ProcEnumerator/CmdOptions.cs
@@ -1,3 +1,9 @@
+/*
+* Author: Bimalesh Jha (46f6780b4f1beba64cf09a4fee5f0d657bc55a37c3a8feaba3023a5ac2f36d87).
+* You may use, modify and distribute this code under the Apache License V2 (https://www.apache.org/licenses/LICENSE-2.0).
+* This file was originally hosted at https://github.com/bjha96/win_tools.
+*/
+
 using System;
 using CommandLine;
 
